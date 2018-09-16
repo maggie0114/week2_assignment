fbd231213ab7e19c1920430f60af3b013585b08c
maggie0114 <494688426@qq.com>
2018-09-16 23:33:04 +0800

    week2 assignment2

--- week2_assignment2.py
+++ week2_assignment2.py
@@ -1,4 +1,6 @@
 #suppose per SD card can use 2 days and I have 20 days of footage are going to shoot
 >>> totally_days = 20
 >>> available_days = 2
+>>> number_of_SD_cards = totally_days/int(available_days) 
+>>> print(number_of_SD_cards)
 10.0
